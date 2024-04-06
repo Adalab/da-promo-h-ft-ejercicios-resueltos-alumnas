f0b7b437b5a0b59e8747c9784c8ed3e7bc5496f2
ggonzalm <165193381+ggonzalm@users.noreply.github.com>
2024-04-06 17:57:09 +0200

    Add files via upload

---
+++ modulo-1/modulo-1-ejercicio-01-variables-Gloria.ipynb.py
@@ -0,0 +1,95 @@
+
+gatos
+gatos = 8
+​
+print(gatos)
+8
+print(perros)
+cuatro
+print(perros, "mas", perros)
+cuatro mas cuatro
+suma_de_perros = "es igual a 8"
+suma_de_perros
+'es igual a 8'
+print(perros, "mas", perros, suma_de_perros)
+cuatro mas cuatro es igual a 8
+print(perros, "+", perros, "=",8)
+cuatro + cuatro = 8
+pizzas = 2.0
+magdalenas = "2"
+postres = 0
+pizzas = 2.0
+magdalenas = "2"
+postres = 0
+print(type(pizzas and magdalenas))
+<class 'str'>
+print(gatos+pizzas)
+print(gatos-pizzas)
+print(gatos*2)
+10.0
+6.0
+16
+piezas = pizzas*8
+piezas
+16.0
+print(piezas**3)
+4096.0
+superficie = (9**2)
+superficie
+superficie
+81
+print(superficie / gatos)
+10.125
+piezas_por_gato = round(piezas/gatos)
+)
+print(piezas_por_gato)
+2
+)
+print(round(superficie / gatos,1))
+10.1
+print(gatos>pizzas)
+True
+print(gatos<superficie)
+True
+!=gatos)
+print((pizzas*2)!=gatos)
+True
+print(piezas>=pizzas)and(superficie!=0)
+True
+print(piezas>=pizzas)and(piezas<superficie)
+print(piezas>=pizzas)and(piezas<superficie)
+True
+print(pizzas>gatos)
+False
+piezas_por_gato = piezas%gatos
+to)
+print(piezas_por_gato)
+0.0
+print(pizzas<=piezas<superficie)
+True
+)
+print(superficie==0)
+False
+ consulta_dia_cumple = input()
+print("tu cumple es el dia:" , consulta_dia_cumple)
+ consulta_dia_cumple = input()
+print("tu cumple es el dia:" , consulta_dia_cumple)
+ 19
+tu cumple es el dia: 19
+dia_cumple = "tu cumple es el dia: 19"
+​
+print(dia_cumple.strip())
+tu cumple es el dia: 19
+ consulta_mes_cumple = input()
+print("tu cumple es en el mes:" , consulta_mes_cumple)
+ octubre
+tu cumple es en el mes: octubre
+mes_cumple = "tu cumple es en el mes: octubre"
+print(mes_cumple.strip())
+tu cumple es en el mes: octubre
+fecha_cumple = (dia_cumple,mes_cumple.replace("tu cumple es en el mes", "de"))
+print(fecha_cumple)
+('tu cumple es el dia: 19', 'de: octubre')
+print(perros.upper())
+CUATRO
+​
