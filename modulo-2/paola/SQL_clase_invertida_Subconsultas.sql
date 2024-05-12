5d26682596a104ac781626024647d6b553f5a9b0
Paola Sánchez Solorzano <163013265+paolasanchezsolorzano@users.noreply.github.com>
2024-05-12 12:03:27 +0200

    ejercicios subconsultas_completo

--- modulo-2/paola/SQL_clase_invertida_Subconsultas.sql
+++ modulo-2/paola/SQL_clase_invertida_Subconsultas.sql
@@ -16,13 +16,17 @@ Having COUNT(customer_number) >= All ( SELECT COUNT(customer_number) -- DEBE TEN
 
 /*3 usa todas las consultas anteriores para seleccionar el customerNumber, nombre y apellido de las clientas
  asignadas a la ciudad con mayor numero de clientas.*/
- 
-SELECT city, COUNT(customer_number),contact_first_name,contact_last_name
+                                 
+SELECT customer_number, contact_first_name, contact_last_name, city
 FROM customers
-group by city
-Having COUNT(customer_number) >= All ( SELECT COUNT(customer_number) -- DEBE TENER EL IGUAL O NO FUNCIONA >= ??????
-								FROM customers
-								GROUP BY city);   
+WHERE city IN
+(SELECT  city
+FROM customers
+GROUP BY city
+HAVING COUNT(customer_number) >= ALL
+(SELECT COUNT(customer_number) AS numero_clientes
+FROM customers
+GROUP BY city));							
  
  
  /* 4. Queremos ver ahora que empleados tienen algún contrato asignado con alguno de los clientes existentes.
@@ -30,13 +34,17 @@ Having COUNT(customer_number) >= All ( SELECT COUNT(customer_number) -- DEBE TEN
  y lastName como 'Apellido Empleado'*/
  
  SELECT employee_number AS 'Nçumero de empleado', first_name as 'Nombre Empleado', last_name ' Apellidos'
- FROM employees;
- 
- 
-SELECT customer_number,sales_rep_employee_number
-FROM  customers
-
+ FROM employees 
+ WHERE employee_number IN (SELECT sales_rep_employee_number
+							FROM  customers);
  
  /* 5. Queremos ver ahora en cuantas ciudades en las cuales tenemos clientes, no también una oficina de nuestra empresa
  para ello: Selecciona aquellas ciudades como 'ciudad' y los nombres de las empresas como 'nombre de la empresa '
  de la tabla customers, sin repeticiones, que no tengan una oficina en dicha ciudad de la tabla offices.*/
+ 
+ 
+ SELECT city AS 'ciudad', customer_name AS 'nombre de la empresa'
+ FROM customers
+ WHERE city NOT IN ( SELECT ciTy
+					FROM offices);
+ 
