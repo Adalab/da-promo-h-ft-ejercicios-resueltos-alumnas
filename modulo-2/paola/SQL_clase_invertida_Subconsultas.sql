b0de997ad1be8402ffb723636055ba7f7f5a966c
Paola Sánchez Solorzano <163013265+paolasanchezsolorzano@users.noreply.github.com>
2024-05-12 12:01:51 +0200

    ejercicios subconsultas

---
+++ modulo-2/paola/SQL_clase_invertida_Subconsultas.sql
@@ -0,0 +1,42 @@
+USE tienda;
+
+/* 1. Calcula el numero de clientes por cada ciudad.*/ # 2 Aucland -> " dentro de alguna de las dos debe haber un espacio"
+SELECT city, COUNT(customer_number)
+FROM customers
+group by city;  
+
+/* 2. Usando la consulta anterior como subconsulta, selecciona la ciudad con el mayor numero de clientes. ( recordar que siempre que hay un group by la subconsulta se debera hacer en el having) */
+
+SELECT city, COUNT(customer_number)
+FROM customers
+group by city
+Having COUNT(customer_number) >= All ( SELECT COUNT(customer_number) -- DEBE TENER EL IGUAL O NO FUNCIONA >= ??????
+								FROM customers
+								GROUP BY city);   
+
+/*3 usa todas las consultas anteriores para seleccionar el customerNumber, nombre y apellido de las clientas
+ asignadas a la ciudad con mayor numero de clientas.*/
+ 
+SELECT city, COUNT(customer_number),contact_first_name,contact_last_name
+FROM customers
+group by city
+Having COUNT(customer_number) >= All ( SELECT COUNT(customer_number) -- DEBE TENER EL IGUAL O NO FUNCIONA >= ??????
+								FROM customers
+								GROUP BY city);   
+ 
+ 
+ /* 4. Queremos ver ahora que empleados tienen algún contrato asignado con alguno de los clientes existentes.
+ Para ello selecciona employeeNumber como 'Número empleado', firstName como 'nombre Empleado'
+ y lastName como 'Apellido Empleado'*/
+ 
+ SELECT employee_number AS 'Nçumero de empleado', first_name as 'Nombre Empleado', last_name ' Apellidos'
+ FROM employees;
+ 
+ 
+SELECT customer_number,sales_rep_employee_number
+FROM  customers
+
+ 
+ /* 5. Queremos ver ahora en cuantas ciudades en las cuales tenemos clientes, no también una oficina de nuestra empresa
+ para ello: Selecciona aquellas ciudades como 'ciudad' y los nombres de las empresas como 'nombre de la empresa '
+ de la tabla customers, sin repeticiones, que no tengan una oficina en dicha ciudad de la tabla offices.*/
